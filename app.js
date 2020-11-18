1bb6e3bff859fe100d95e856560e4207eeb9066c
coke328 <kyuminhan823@gmail.com>
2020-11-19 00:27:39 +0900

    0.1

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+class block{
+    constructor(x,y,color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+    
+}
+
+var b = new block()[4];
+
 const shape =[
     1,3,5,7,//|
     2,4,5,7,//z
@@ -9,16 +20,7 @@ const shape =[
     2,3,5,7,
     3,5,7,6,
     2,3,4,5
-]
-
-class block{
-    constructor(x,y,color){
-        this.x = x;
-        this.y = y;
-        this.color = color;
-    }
-
-}
+];
 
 function drawblock(color){//color 0,1,2..8
 
